0b58b6355c4d03e920cc26649bdf2efaa776faa6
baifumei <k_katerina_k@icloud.com>
2022-08-05 17:29:40 +0300

    Change content view struct

--- Sign up design/ContentView.swift
+++ Sign up design/ContentView.swift
@@ -10,66 +10,62 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         GeometryReader { _ in
-            ZStack {
-                VStack(spacing: 15) {
-                    HStack {
-                        Image("logo")
-                            .resizable()
-                            .frame(width: 150, height: 150)
-                    }
-                    ZStack {
-                        //
-                    }
-                    
-                    Spacer()
-                    
-                    VStack {
-                        HStack(spacing: 5) {
-                            Rectangle()
-                                .frame(height: 2)
-                                .foregroundColor(Color("Color3"))
-                            Text("OR")
-                                .font(.subheadline)
-                            Rectangle()
-                                .frame(height: 2)
-                                .foregroundColor(Color("Color3"))
-                        }.padding(5)
-                    }
-                    VStack {
-                        HStack(spacing: 25) {
-                            Button(action: {
-                                //
-                            }) {
+            VStack(spacing: 15) {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                ZStack {
+                    //
+                }
+                
+                Spacer()
+                
+                VStack {
+                    HStack(spacing: 5) {
+                        Rectangle()
+                            .frame(height: 2)
+                            .foregroundColor(Color("Color3"))
+                        Text("OR")
+                            .font(.subheadline)
+                        Rectangle()
+                            .frame(height: 2)
+                            .foregroundColor(Color("Color3"))
+                    }.padding(5)
+                }
+                VStack {
+                    HStack(spacing: 25) {
+                        Button(action: {
+                            //
+                        }) {
                             Image("AppleLogo")
                                 .resizable()
                                 .frame(width: 35, height: 45)
                                 .offset(y: -3)
-                            }
-                            Button(action: {
-                                //
-                            }) {
+                        }
+                        Button(action: {
+                            //
+                        }) {
                             Image("FacebookLogo")
                                 .resizable()
                                 .frame(width: 45, height: 45)
-                            }
-                            Button(action: {
-                                //
-                            }) {
+                        }
+                        Button(action: {
+                            //
+                        }) {
                             Image("TwitterLogo")
                                 .resizable()
                                 .frame(width: 45, height: 35)
                                 .offset(y: 3)
-                            }
-                            Button(action: {
-                                //
-                            }) {
+                        }
+                        Button(action: {
+                            //
+                        }) {
                             Image("VKLogo")
                                 .resizable()
                                 .frame(width: 45, height: 45)
-                            }
                         }
-                    }.padding(.bottom)
-                }
+                    }
+                }.padding(.bottom)
             }
         }.background(Color("Color4"))
     }
