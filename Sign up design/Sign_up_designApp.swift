bc622064cc636b55071930a7d2584a47d577ce82
baifumei <k_katerina_k@icloud.com>
2022-08-05 15:53:15 +0300

    Initial Commit

---
+++ Sign up design/Sign_up_designApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sign_up_designApp.swift
+//  Sign up design
+//
+//  Created by Екатерина К on 05.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Sign_up_designApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
